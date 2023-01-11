b032bc5730a60d5f58fce7ee30e33c595549d7cd
Luke <lukewward95@gmail.com>
2023-01-11 10:42:08 +0000

    Completed second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(mesg, numOfTimes) {
+    if (numOfTimes >= 0){
+        let newMesg = ""
+        for(i = 0; i < numOfTimes; i++){
+            newMesg = newMesg + mesg
+        }
+        return newMesg
+    } else {
+        return "ERROR"
+    }
+  
 };
 
 // Do not edit below this line
